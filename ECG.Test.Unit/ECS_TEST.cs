aeb7039181a6dcef3460bffb6ea59d4efe9c13c9
Martin Bojesen <Au616699@uni.au.dk>
2020-02-12 15:24:26 +0100

    Tilføjet ClassLibrary

---
+++ ECG.Test.Unit/ECS_TEST.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ECS.Test.Unit
+{
+   [TestFixture]
+    public class ECS_TEST
+    {
+
+       [SetUp]
+       public void Setup()
+       {
+
+       }
+
+
+    }
+}
